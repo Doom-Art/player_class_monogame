6161289f30a912c79bc736e7c95cd63e04e6e090
Doom-Art <ouginak121@gmail.com>
2022-12-12 08:55:51 -0500

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+
+namespace player_class_monogame
+{
+    internal class Player
+    {
+        private Texture2D _texture;
+        private Rectangle _location;
+        private Vector2 _speed;
+
+        public Player(Texture2D texture, int x, int y)
+        {
+            _texture = texture;
+            _location = new Rectangle(x, y, 30, 30);
+            _speed = new Vector2();
+        }
+        public float HSpeed
+        {
+            get { return _speed.X; }
+            set { _speed.X = value; }
+        }
+        public float VSpeed
+        {
+            get { return _speed.Y; }
+            set { _speed.Y = value; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new player_class_monogame.Game1();
+game.Run();
